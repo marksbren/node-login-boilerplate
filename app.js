0519d08dba93ecfa77bee92caf4589b14672c2f9
marksbren <marksbren@gmail.com>
2022-12-12 19:32:06 -0800

    fixing

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ var express = require('express');
 var passport = require('passport');
 var app = express();
 
-
 const mongoose = require('mongoose');
-
 mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGODB_URL, {
 	useNewUrlParser: true,
@@ -24,7 +22,6 @@ var authRouter = require('./routes/auth');
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/', authRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
